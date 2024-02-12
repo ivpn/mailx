9b34017ef5aff88ec231cccbe0f0d17a5fbbe9cc
Juraj Hilje <juraj.hilje@gmail.com>
2024-02-12 21:02:30 +0100

    refactor: clean up routes.go

--- internal/transport/api/routes.go
+++ internal/transport/api/routes.go
@@ -1,11 +1,7 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
-
 func (h *Handler) SetupRoutes() {
 	v1 := h.Server.Group("/v1")
-
-	v1.Get("/health", HealthCheck)
 
 	v1.Get("/recipient/:id", h.GetRecipient)
 	v1.Get("/recipients/:user_id", h.GetRecipients)
@@ -19,8 +15,4 @@ func (h *Handler) SetupRoutes() {
 	v1.Post("/alias", h.PostAlias)
 	v1.Put("/alias", h.UpdateAlias)
 	v1.Delete("/alias/:id", h.DeleteAlias)
-}
-
-func HealthCheck(c *fiber.Ctx) error {
-	return c.SendString("OK")
 }

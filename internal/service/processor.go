222404ed15cb15ae3d5a5a57423d9e027e45a0f1
Juraj Hilje <juraj.hilje@gmail.com>
2024-02-13 20:12:40 +0100

    feat(service): update processor.go

--- internal/service/processor.go
+++ internal/service/processor.go
@@ -2,20 +2,39 @@ package service
 
 import (
 	"bytes"
+	"context"
 	"log"
 	"net"
 	"net/mail"
+	"strings"
 )
 
 func (s *Service) ProcessMessage(origin net.Addr, from string, to []string, data []byte) error {
 	msg, err := mail.ReadMessage(bytes.NewReader(data))
 	if err != nil {
-		log.Print(err)
+		log.Println("[Service] Drop message: " + err.Error())
 	}
 
 	log.Printf("[Service] From: %s", from)
 	log.Printf("[Service] To: %s", to)
 	log.Printf("[Service] Subject: %s", msg.Header.Get("Subject"))
 
+	name := getName(to[0])
+	alias, err := s.GetAliasByName(name)
+	if err != nil {
+		log.Println("[Service] Drop message: " + err.Error())
+	}
+
+	recipient, err := s.GetRecipient(context.Background(), alias.RecipientID)
+	if err != nil {
+		log.Println("[Service] Drop message: " + err.Error())
+	}
+
+	log.Println("[Service] Recipient:" + recipient.Email)
+
 	return err
+}
+
+func getName(email string) string {
+	return email[:strings.Index(email, "@")]
 }

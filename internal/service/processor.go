1b7758bd85e791859a73c752dfe487714336ef5e
Juraj Hilje <juraj.hilje@gmail.com>
2024-02-13 20:24:58 +0100

    feat(service): update processor.go

--- internal/service/processor.go
+++ internal/service/processor.go
@@ -13,24 +13,29 @@ func (s *Service) ProcessMessage(origin net.Addr, from string, to []string, data
 	msg, err := mail.ReadMessage(bytes.NewReader(data))
 	if err != nil {
 		log.Println("[Service] Drop message: " + err.Error())
+		return err
 	}
 
 	log.Printf("[Service] From: %s", from)
 	log.Printf("[Service] To: %s", to)
 	log.Printf("[Service] Subject: %s", msg.Header.Get("Subject"))
 
-	name := getName(to[0])
-	alias, err := s.GetAliasByName(name)
-	if err != nil {
-		log.Println("[Service] Drop message: " + err.Error())
-	}
+	for _, to := range to {
+		name := getName(to)
+		alias, err := s.GetAliasByName(name)
+		if err != nil {
+			log.Println("[Service] Drop message: " + err.Error())
+			continue
+		}
 
-	recipient, err := s.GetRecipient(context.Background(), alias.RecipientID)
-	if err != nil {
-		log.Println("[Service] Drop message: " + err.Error())
-	}
+		recipient, err := s.GetRecipient(context.Background(), alias.RecipientID)
+		if err != nil {
+			log.Println("[Service] Drop message: " + err.Error())
+			continue
+		}
 
-	log.Println("[Service] Recipient: " + recipient.Email)
+		log.Println("[Service] Recipient: " + recipient.Email)
+	}
 
 	return err
 }

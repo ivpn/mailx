1e3f43a084f23efa77fe475c06e2798c7db757e5
Juraj Hilje <juraj.hilje@gmail.com>
2024-02-15 15:53:22 +0100

    feat(client): create email.go

---
+++ internal/client/email/email.go
@@ -0,0 +1,42 @@
+package email
+
+import (
+	"log"
+	"strconv"
+
+	"gopkg.in/gomail.v2"
+	"ivpn.net/email-service/config"
+)
+
+func Send(from string, to string, subject string, body string) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	port, err := strconv.Atoi(cfg.SMTPClient.Port)
+	if err != nil {
+		return err
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/plain", body)
+
+	d := gomail.Dialer{
+		Host:     cfg.SMTPClient.Host,
+		Port:     port,
+		Username: cfg.SMTPClient.User,
+		Password: cfg.SMTPClient.Password,
+	}
+	err = d.DialAndSend(m)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Email sent successfully")
+
+	return nil
+}

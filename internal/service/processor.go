82807e706b50268c5f70d568860bf7b32ea118f1
Juraj Hilje <juraj.hilje@gmail.com>
2024-02-15 16:55:59 +0100

    feat(service): update processor.go

--- internal/service/processor.go
+++ internal/service/processor.go
@@ -7,39 +7,70 @@ import (
 	"net"
 	"net/mail"
 	"strings"
+
+	"ivpn.net/email-service/internal/client/email"
 )
 
+type Message struct {
+	From    string
+	To      []string
+	Subject string
+	Body    string
+}
+
 func (s *Service) ProcessMessage(origin net.Addr, from string, to []string, data []byte) error {
-	msg, err := mail.ReadMessage(bytes.NewReader(data))
+	msg, err := parse(origin, from, to, data)
 	if err != nil {
 		log.Println("[Service] Drop message: " + err.Error())
 		return err
 	}
 
-	log.Printf("[Service] From: %s", from)
-	log.Printf("[Service] To: %s", to)
-	log.Printf("[Service] Subject: %s", msg.Header.Get("Subject"))
-
-	for _, to := range to {
-		name := getName(to)
-		alias, err := s.GetAliasByName(name)
+	for _, to := range msg.To {
+		recipient, err := s.findRecipient(to)
 		if err != nil {
 			log.Println("[Service] Drop message: " + err.Error())
 			continue
 		}
 
-		recipient, err := s.GetRecipient(context.Background(), alias.RecipientID)
+		err = email.Send(msg.From, recipient, msg.Subject, msg.Body)
 		if err != nil {
-			log.Println("[Service] Drop message: " + err.Error())
-			continue
+			log.Println("[Service] Error sending message: " + err.Error())
 		}
-
-		log.Println("[Service] Recipient: " + recipient.Email)
 	}
 
 	return err
 }
 
-func getName(email string) string {
-	return email[:strings.Index(email, "@")]
+func (s *Service) findRecipient(email string) (string, error) {
+	name := email[:strings.Index(email, "@")]
+	alias, err := s.GetAliasByName(name)
+	if err != nil {
+		return "", err
+	}
+
+	recipient, err := s.GetRecipient(context.Background(), alias.RecipientID)
+	if err != nil {
+		return "", err
+	}
+
+	return recipient.Email, nil
+}
+
+func parse(origin net.Addr, from string, to []string, data []byte) (Message, error) {
+	msg, err := mail.ReadMessage(bytes.NewReader(data))
+	if err != nil {
+		return Message{}, err
+	}
+
+	subject := msg.Header.Get("Subject")
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(msg.Body)
+	body := buf.String()
+
+	return Message{
+		From:    from,
+		To:      to,
+		Subject: subject,
+		Body:    body,
+	}, nil
 }

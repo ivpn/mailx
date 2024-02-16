551fd097771f4677b409c96b42cf8f7cc25fcf85
Juraj Hilje <juraj.hilje@gmail.com>
2024-02-16 15:21:30 +0100

    feat(model): update user.go

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,10 +2,13 @@ package model
 
 import (
 	"errors"
+	"regexp"
 )
 
 var (
-	ErrDuplicateEmail = errors.New("duplicate email")
+	ErrEmailInvalid    = errors.New("invalid email")
+	ErrPasswordInvalid = errors.New("invalid password")
+	EmailRX            = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 )
 
 type User struct {
@@ -17,4 +20,16 @@ type User struct {
 type password struct {
 	plaintext *string
 	hash      string
+}
+
+func (u *User) IsValid() error {
+	if u.Email == "" || !EmailRX.MatchString(u.Email) {
+		return ErrEmailInvalid
+	}
+
+	if u.Password.plaintext == nil || len(*u.Password.plaintext) < 8 || len(*u.Password.plaintext) > 64 {
+		return ErrPasswordInvalid
+	}
+
+	return nil
 }

142a86b843757dede059ae3dfed08661853a413f
Juraj Hilje <juraj.hilje@gmail.com>
2024-02-25 11:24:32 +0100

    feat(model): update alias.go

--- internal/model/alias.go
+++ internal/model/alias.go
@@ -8,6 +8,7 @@ var (
 
 type Alias struct {
 	BaseModel
+	UserID      string `json:"user_id"`
 	RecipientID string `json:"recipient_id"`
 	Name        string `gorm:"unique" json:"name"`
 	Descripion  string `json:"description"`

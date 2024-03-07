36f61f2a6bee1fee7a5d219d3265d2543b2d5770
Juraj Hilje <juraj.hilje@gmail.com>
2024-03-07 17:00:40 +0100

    feat(api): update routes.go

--- internal/transport/api/routes.go
+++ internal/transport/api/routes.go
@@ -25,7 +25,7 @@ func (h *Handler) SetupRoutes(cfg config.APIConfig) {
 
 	v1.Get("/alias/:id", h.GetAlias)
 	v1.Get("/aliases", h.GetAliases)
-	v1.Post("/alias/", h.PostAlias)
+	v1.Post("/alias", h.PostAlias)
 	v1.Put("/alias/:id", h.UpdateAlias)
 	v1.Delete("/alias/:id", h.DeleteAlias)
 }

b8b38c405ac891c401c465c917250298c4f374b0
Juraj Hilje <juraj.hilje@gmail.com>
2024-04-18 13:30:55 +0200

    feat(app): update Signup.vue

--- app/src/components/Signup.vue
+++ app/src/components/Signup.vue
@@ -3,45 +3,84 @@
         <h1 class="text-gray-700 text-2xl font-semibold mb-2">Sign Up</h1>
         <p class="text-gray-500 text-sm mb-8">Have an account? <a class="text-blue-500 hover:text-blue-700" href="/login">Log In</a></p>
         <form class="w-full max-w-sm bg-white rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="register">
-            <div class="mb-4">
-                <label class="block text-gray-500 text-sm font-bold mb-2" for="email">
-                    Email Address
-                </label>
-                <input v-model="email"
-                    class="appearance-none outline-none border-2 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:border-blue-500 mb-2"
-                    id="email" type="email" autocomplete="email">
-                    <p class="text-red-500 text-sm">This field is required</p>
+            <div v-if="!apiSuccess">
+                <div class="mb-4">
+                    <label class="block text-gray-500 text-sm font-bold mb-2" for="email">
+                        Email Address
+                    </label>
+                    <input
+                        v-model="email"
+                        v-bind:class="{ 'border-red-500': emailError }"
+                        class="appearance-none outline-none border-2 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:border-blue-500 mb-2"
+                        id="email" type="email" autocomplete="email">
+                        <p v-if="emailError" class="text-red-500 text-sm">This field is required</p>
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-500 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input
+                        v-model="password"
+                        v-bind:class="{ 'border-red-500': passwordError }"
+                        class="appearance-none outline-none border-2 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:border-blue-500 mb-2"
+                        id="password" type="password" autocomplete="current-password">
+                    <p v-if="passwordError" class="text-red-500 text-sm mb-2">This field is required</p>
+                    <p class="text-gray-500 text-sm mb-2">Must be at least 8 characters</p>
+                </div>
+                <div class="flex items-center justify-between">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        Sign Up
+                    </button>
+                </div>
+                <p v-if="apiError" class="text-red-500 text-sm mt-6">{{ apiError }}</p>
             </div>
-            <div class="mb-6">
-                <label class="block text-gray-500 text-sm font-bold mb-2" for="password">
-                    Password
-                </label>
-                <input v-model="password"
-                    class="appearance-none outline-none border-2 border-red-500 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:border-blue-500 mb-2"
-                    id="password" type="password" autocomplete="current-password">
-                <p class="text-red-500 text-sm mb-2">This field is required</p>
-                <p class="text-gray-500 text-sm mb-2">Must be at least 8 characters</p>
+            <div v-if="apiSuccess">
+                <p class="text-gray-500 text-sm mb-6">{{ apiSuccess }}</p>
+                <a
+                    href="/login"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline">
+                    Proceed to Log In
+                </a>
             </div>
-            <div class="flex items-center justify-between">
-                <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="submit">
-                    Sign Up
-                </button>
-            </div>
-            <p class="text-red-500 text-sm mt-6">Error message from the API.</p>
         </form>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import axios from 'axios'
 import { userApi } from '../api/user.ts'
 
 const email = ref('')
 const password = ref('')
+const emailError = ref(false)
+const passwordError = ref(false)
+const apiSuccess = ref('')
+const apiError = ref('')
+
+const validateEmail = () => {
+    emailError.value = !email.value
+    return !emailError.value
+}
+
+const validatePassword = () => {
+    passwordError.value = !password.value
+    return !passwordError.value
+}
+
+const validate = () => {
+    const validEmail = validateEmail()
+    const validPass = validatePassword()
+    return validEmail && validPass
+}
 
 const register = async () => {
+    if (!validate()) {
+        return
+    }
+    
     const data = {
         email: email.value,
         password: password.value
@@ -49,9 +88,13 @@ const register = async () => {
 
     try {
         const response = await userApi.register(data)
-        console.log(response)
+        apiSuccess.value = response.data.message
+        apiError.value = ''
     } catch (error) {
-        console.error(error)
+        apiSuccess.value = ''
+        if (axios.isAxiosError(error)) {
+            apiError.value = error.message
+        }
     }
 }
 </script>

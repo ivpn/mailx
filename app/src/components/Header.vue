4b89f9f661f24b789df0a5f1ef97f763f29ca0e9
Juraj Hilje <juraj.hilje@gmail.com>
2024-04-20 17:46:59 +0200

    feat(app): create Menu.vue

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -15,39 +15,10 @@
                 </svg>
             </button>
         </div>
-        <nav>
-            <div class="flex flex-row items-center gap-6">
-                <a
-                v-bind:class="{ 'text-blue-600 border-blue-600 border-b-2': route == '/', 'text-gray-500': route != '/'}"
-                class="py-4 font-medium hover:text-blue-600" href="/">Dashboard</a>
-                <a
-                v-bind:class="{ 'text-blue-600 border-blue-600 border-b-2': route == '/aliases', 'text-gray-500': route != '/aliases'}"
-                class="py-4 font-medium hover:text-blue-600" href="/aliases">Aliases</a>
-                <a
-                v-bind:class="{ 'text-blue-600 border-blue-600 border-b-2': route == '/recipients', 'text-gray-500': route != '/recipients'}"
-                class="py-4 font-medium hover:text-blue-600" href="/recipients">Recipients</a>
-                <a
-                v-bind:class="{ 'text-blue-600 border-blue-600 border-b-2': route == '/stats', 'text-gray-500': route != '/stats'}"
-                class="py-4 font-medium hover:text-blue-600" href="/stats">Stats</a>
-                <a
-                v-bind:class="{ 'text-blue-600 border-blue-600 border-b-2': route == '/settings', 'text-gray-500': route != '/settings'}"
-                class="py-4 font-medium hover:text-blue-600"  href="/settings">Settings</a>
-                <a
-                v-bind:class="{ 'text-blue-600 border-blue-600 border-b-2': route == '/subscription', 'text-gray-500': route != '/subscription'}"
-                class="py-4 font-medium hover:text-blue-600"  href="/subscription">Subscription</a>
-            </div>
-        </nav>
+        <Menu />
     </header>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-
-const route = ref('/')
-const currentRoute = useRoute()
-
-watch(currentRoute, (newRoute) => {
-  route.value = newRoute.path
-}, { immediate: true })
+import Menu from './Menu.vue'
 </script>

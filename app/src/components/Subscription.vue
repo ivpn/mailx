7767751092ac25d14f223cecea64c2c249400bd7
Juraj Hilje <juraj.hilje@gmail.com>
2024-04-22 10:08:30 +0200

    feat(app): update Subscription.vue

--- app/src/components/Subscription.vue
+++ app/src/components/Subscription.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex flex-col bg-white border border-gray-200 rounded-xl p-5 pb-4 my-8">
         <h1 class="text-lg font-bold text-gray-800 mb-4">Subscription</h1>
-        <p class="text-sm text-gray-500 mb-3">
+        <p v-if="res.id" class="text-sm text-gray-500 mb-3">
             <span v-if="isActive()" class="inline-flex items-center py-1.5 px-2 rounded-md text-xs font-medium bg-teal-100 text-teal-800">Active</span>
             <span v-if="!isActive()" class="inline-flex items-center py-1.5 px-2 rounded-md text-xs font-medium bg-gray-100 text-gray-500">Inactive</span>
         </p>
-        <p class="text-sm text-gray-500 mb-3">Subscription ID:
+        <p v-if="res.id" class="text-sm text-gray-500 mb-3">Subscription ID:
             <span class="text-black font-semibold">{{ res.id }}</span>
         </p>
         <p v-if="apiError" class="text-red-500 text-sm mb-3">{{ apiError }}</p>

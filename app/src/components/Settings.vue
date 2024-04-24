440ded217f4d756c6282ac0144cbfdffe02aad1f
Juraj Hilje <juraj.hilje@gmail.com>
2024-04-24 14:46:38 +0200

    feat(app): update Settings.vue

--- app/src/components/Settings.vue
+++ app/src/components/Settings.vue
@@ -14,16 +14,20 @@
             </label>
             <select id="domain" 
                 class="form-select py-3 px-4 pe-9 block w-full border-2 border-gray-200 rounded-lg text-gray-700 focus:border-violet-600 disabled:opacity-50 disabled:pointer-events-none outline-none">
-                <option>Domain 1</option>
-                <option>Domain 2</option>
-                <option>Domain 3</option>
+                <option
+                    v-for="domain in domains"
+                    v-bind:value=domain
+                    :selected="domain == res.domain"
+                    :key="domain">
+                    {{ domain }}
+                </option>
             </select>
         </div>
         <h2 class="font-semibold text-gray-800 mb-3">
             Default Recipient
         </h2>
         <p class="text-sm text-gray-500 mb-3">
-            The default recipient to be selected by default in the drop down options when creating a new recipient.
+            The default recipient to be selected by default in the drop down options when creating a new recipient. You can create recipients <a class="text-violet-600 hover:text-violet-700 font-semibold" href="/recipients">here</a>.
         </p>
         <div class="max-w-xs mb-6">
             <label class="block text-gray-500 text-sm font-semibold mb-3" for="recipient">
@@ -47,7 +51,7 @@
             <label class="block text-gray-500 text-sm font-semibold mb-3" for="from-name">
                 From name:
             </label>
-            <input
+            <input v-model="res.from_name"
                 class="appearance-none outline-none border-2 rounded-md w-full py-3 px-4 text-gray-700 leading-tight focus:border-violet-600 mb-2"
                 id="from-name" type="text">
         </div>
@@ -58,8 +62,36 @@
                 Save Settings
             </button>
         </div>
+        <p v-if="error" class="text-red-500 text-sm mb-3">{{ error }}</p>
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import { settingsApi } from '../api/settings.ts'
+import env from "../env.json"
+
+const res = ref({
+    domain: '',
+    recipient: '',
+    from_name: ''
+})
+const error = ref('')
+const domains = ref(env.DOMAINS)
+
+const getSettings = async () => {
+    try {
+        const response = await settingsApi.get()
+        res.value = response.data
+    } catch (err) {
+        if (axios.isAxiosError(err)) {
+            error.value = err.message
+        }
+    }
+}
+
+onMounted(() => {
+    getSettings()
+})
 </script>

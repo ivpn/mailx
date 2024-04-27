7a3f263d865638e2c60fa171713950b6ed2e157b
Juraj Hilje <juraj.hilje@gmail.com>
2024-04-27 21:16:04 +0200

    feat(app): update AliasCard.vue

--- app/src/components/AliasCard.vue
+++ app/src/components/AliasCard.vue
@@ -3,7 +3,7 @@
         <td class="pr-5 py-4 whitespace-nowrap text-start text-sm text-gray-800">
             <div class="mt-1 flex items-center gap-2">
                 <div v-if="alias.enabled" class="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+                    <div class="h-1.5 w-1.5 rounded-full bg-emerald-400"></div>
                 </div>
                 <div v-if="!alias.enabled" class="flex-none rounded-full bg-gray-500/20 p-1">
                     <div class="h-1.5 w-1.5 rounded-full bg-gray-400"></div>
@@ -34,7 +34,7 @@
         </td>
         <td class="px-5 py-4 whitespace-nowrap text-start text-sm">
             <div class="flex items-center">
-                <input type="checkbox" v-bind:checked="alias.enabled"
+                <input type="checkbox" v-bind:checked="alias.enabled" @change="updateAlias"
                     class="form-checkbox relative w-11 h-6 p-px bg-gray-100 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-white disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-violet-600 checked:border-violet-600 
 
                     before:inline-block before:size-5 before:bg-white checked:before:bg-violet-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow before:transform before:ring-0 before:transition before:ease-in-out before:duration-200">
@@ -58,14 +58,22 @@
 import { ref, onMounted, defineEmits } from 'vue'
 import tooltip from '@preline/tooltip'
 import AliasEdit from './AliasEdit.vue'
+import { aliasApi } from '../api/alias.ts'
 
 const props = defineProps(['alias', 'recipients'])
-let alias = props.alias
+let alias = ref(props.alias)
 const recipients = ref(props.recipients)
 const emit = defineEmits(['onDeleteAlias'])
 
+const updateAlias = async () => {
+    alias.value.enabled = !alias.value.enabled
+    try {
+        await aliasApi.update(alias.value.id, alias.value)
+    } catch {}
+}
+
 const deleteAlias = () => {
-    emit('onDeleteAlias', alias.id)
+    emit('onDeleteAlias', alias.value.id)
 }
 
 onMounted(() => {

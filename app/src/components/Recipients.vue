e290739d59b05443c17b2b8336b10b17f4d4807f
Juraj Hilje <juraj.hilje@gmail.com>
2024-04-30 15:22:30 +0200

    feat(app): update Recipients.vue

--- app/src/components/Recipients.vue
+++ app/src/components/Recipients.vue
@@ -1,5 +1,77 @@
 <template>
-    <div>
-        <h1>Recipients</h1>
+    <div class="flex flex-col p-5 pb-4 my-8">
+        <div class="flex flex-col items-center p-4 text-center py-20">
+            <h3 class="text-lg font-bold text-gray-800">
+                No recipients yet
+            </h3>
+            <p class="my-2 text-gray-500">
+                To get started, add a recipient.
+            </p>
+            <div class="flex gap-4">
+                <button
+                    class="mt-3 py-2 pl-2 pr-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-transparent bg-violet-600 text-white hover:bg-violet-700">
+                    <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5v14"></path>
+                    </svg>
+                    Add Recipient
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col bg-white shadow-sm rounded-xl p-5 pb-4 my-8">
+        <h1 class="text-2xl font-bold text-gray-800 mb-5">Recipients</h1>
+        <div>
+            <div class="flex items-center justify-between mb-6">
+                <button
+                    class="mt-3 py-2 pl-2 pr-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-transparent bg-violet-600 text-white hover:bg-violet-700">
+                    <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5v14"></path>
+                    </svg>
+                    Add Recipient
+                </button>
+            </div>
+            <div class="flex flex-col">
+                <div class="-m-1.5 overflow-x-auto">
+                    <div class="p-1.5 min-w-full inline-block align-middle">
+                        <div class="overflow-hidden">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead>
+                                    <tr>
+                                        <th scope="col"
+                                            class="pr-5 py-3 text-start text-xs font-medium text-gray-500">
+                                            CREATED</th>
+                                        <th scope="col"
+                                            class="px-5 py-3 text-start text-xs font-medium text-gray-500">
+                                            EMAIL</th>
+                                        <th scope="col"
+                                            class="px-5 py-3 text-start text-xs font-medium text-gray-500">
+                                            VERIFIED</th>
+                                        <th scope="col"
+                                            class="pl-5 py-3 text-end text-xs font-medium text-gray-500">
+                                            ACTIONS</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200">
+                                    <!-- TODO: Recipient card -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p v-if="error" class="text-red-600 text-sm mb-4">{{ error }}</p>
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const error = ref('')
+</script>

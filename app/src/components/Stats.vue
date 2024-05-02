cbe09a85cdc59b776ded3d65818522ef45d9a4ff
Juraj Hilje <juraj.hilje@gmail.com>
2024-05-02 20:44:04 +0200

    feat(app): update Stats.vue

--- app/src/components/Stats.vue
+++ app/src/components/Stats.vue
@@ -1,12 +1,25 @@
 <template>
     <div class="flex flex-col bg-white shadow-sm rounded-xl p-5 pb-4 my-8">
-        <h1 class="text-2xl font-bold text-gray-800 mb-5">Stats</h1>
+        <h1 class="text-2xl font-bold text-gray-800 mb-5">Last 90 days</h1>
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4 text-center mb-8">
+            <div class="p-4 border-r border-gray-200">
+                <p class="text-4xl font-bold text-gray-800 mb-2">{{ stats.forwards }}</p>
+                <p class="text-gray-500">Forwards</p>
+            </div>
+            <div class="p-4 border-r border-white md:border-gray-200">
+                <p class="text-4xl font-bold text-gray-800 mb-2">{{ stats.blocks }}</p>
+                <p class="text-gray-500">Blocks</p>
+            </div>
+            <div class="p-4 border-r border-gray-200">
+                <p class="text-4xl font-bold text-gray-800 mb-2">{{ stats.replies }}</p>
+                <p class="text-gray-500">Replies</p>
+            </div>
+            <div class="p-4">
+                <p class="text-4xl font-bold text-gray-800 mb-2">{{ stats.sends }}</p>
+                <p class="text-gray-500">Sends</p>
+            </div>
+        </div>
         <div class="mb-5">
-            <p>Forwards: {{ stats.forwards }}</p>
-            <p>Blocks: {{ stats.blocks }}</p>
-            <p>Replies: {{ stats.replies }}</p>
-            <p>Sends: {{ stats.sends }}</p>
-            <p>Bandwidth: {{ getBandwidth() }}kb</p>
             <p>Aliases: {{ stats.aliases }}</p>
         </div>
         <p v-if="error" class="text-red-600 text-sm mb-3">Error: {{ error }}</p>
@@ -42,10 +55,6 @@ const getStats = async () => {
             error.value = err.response?.data.error || err.message
         }
     }
-}
-
-const getBandwidth = () => {
-    return (stats.value.bandwidth / 1024).toFixed(2)
 }
 
 onMounted(() => {

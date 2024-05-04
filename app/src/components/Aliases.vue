338e010b9b501636719365c8f51e1a736a816d8b
Juraj Hilje <juraj.hilje@gmail.com>
2024-05-04 17:43:41 +0200

    feat(app): update Aliases.vue

--- app/src/components/Aliases.vue
+++ app/src/components/Aliases.vue
@@ -17,7 +17,7 @@
     </div>
     <div v-bind:class="{ 'hidden': !list.length || !loaded }" class="flex flex-col bg-white shadow-sm rounded-xl p-5 pb-4 my-8">
         <h1 v-if="!isDashboard" class="text-2xl font-bold text-gray-800 mb-5">Aliases</h1>
-        <h1 v-if="isDashboard" class="text-2xl font-bold text-gray-800 mb-5">Recent Aliases</h1>
+        <h1 v-if="isDashboard" class="text-2xl font-bold text-gray-800 mb-5">Latest Aliases</h1>
         <div>
             <div class="flex items-center justify-between mb-6">
                 <AliasCreate v-if="recipients.length && settings.recipient" @onCreateAlias="getList" :recipients.sync="recipients" :settings.sync="settings" />
--- app/src/components/Recipients.vue
+++ app/src/components/Recipients.vue
@@ -14,7 +14,7 @@
     </div>
     <div v-bind:class="{ 'hidden': !list.length || !loaded }" class="flex flex-col bg-white shadow-sm rounded-xl p-5 pb-4 my-8">
         <h1 v-if="!isDashboard" class="text-2xl font-bold text-gray-800 mb-5">Recipients</h1>
-        <h1 v-if="isDashboard" class="text-2xl font-bold text-gray-800 mb-5">Recent Recipients</h1>
+        <h1 v-if="isDashboard" class="text-2xl font-bold text-gray-800 mb-5">Latest Recipients</h1>
         <div>
             <div class="flex items-center justify-between mb-6">
                 <RecipientCreate @onCreateRecipient="getList" />

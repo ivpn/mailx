9c507ff7e870c6404175345f6b03cce734a7b8c0
Juraj Hilje <juraj.hilje@gmail.com>
2024-05-07 10:32:34 +0200

    refactor(utils): clean up validator.go

--- api/internal/utils/validator.go
+++ api/internal/utils/validator.go
@@ -1,17 +1,7 @@
 package utils
 
 import (
-	"errors"
-	"regexp"
-
 	"github.com/go-playground/validator/v10"
-)
-
-var (
-	ErrInvalidEmail    = errors.New("invalid email")
-	ErrInvalidPassword = errors.New("invalid password")
-	ErrInvalidOTP      = errors.New("invalid otp")
-	EmailRX            = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 )
 
 type Validator struct {

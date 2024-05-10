91c5544e40995658c40957a83f43f97fe6370bce
Juraj Hilje <juraj.hilje@gmail.com>
2024-05-10 09:44:54 +0200

    feat(repository): create obs/user.go

--- api/internal/cron/cron.go
+++ api/internal/cron/cron.go
@@ -1,6 +1,8 @@
 package cron
 
 import (
+	"log"
+
 	"github.com/jasonlvhit/gocron"
 	"gorm.io/gorm"
 	"ivpn.net/email/api/internal/cron/jobs"
@@ -9,5 +11,7 @@ import (
 func New(db *gorm.DB) {
 	gocron.Every(1).Hour().Do(jobs.DeleteOldMessages, db)
 	gocron.Every(1).Hour().Do(jobs.DeleteUnverifiedRecipients, db)
+	gocron.Every(1).Hour().Do(jobs.DeleteUnverifiedUsers, db)
 	gocron.Start()
+	log.Println("Cron jobs started")
 }

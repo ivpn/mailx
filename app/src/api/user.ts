4c4ba5c481b184429316d3afbddf117192f1ce42
Juraj Hilje <juraj.hilje@gmail.com>
2024-05-14 20:23:43 +0200

    feat(app): update api/user.ts

--- app/src/api/user.ts
+++ app/src/api/user.ts
@@ -1,8 +1,8 @@
 import { api } from './api'
 
 export const userApi = {
-    register: (data: any) => api.post('/register', data),
-    login: (data: any) => api.post('/login', data),
+    register: (data: any) => api.post('/p/register', data),
+    login: (data: any) => api.post('/p/login', data),
     logout: () => api.post('/user/logout'),
     delete: (data: any) => api.delete('/user/delete', data),
     sendOtp: () => api.post('/user/sendotp'),

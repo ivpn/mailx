b6d1ac198dc655b5d7ff7db8bf64696444dd6410
Juraj Hilje <juraj.hilje@gmail.com>
2024-06-05 15:22:36 +0200

    feat(app): update tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,7 +6,23 @@ export default {
     "./node_modules/preline/preline.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bluish: {
+          50: '#eff8ff',
+          100: '#dbeefe',
+          200: '#bee2ff',
+          300: '#92d1fe',
+          400: '#5fb6fb',
+          500: '#449cf8',
+          600: '#2378ed',
+          700: '#1b62da',
+          800: '#1d4fb0',
+          900: '#1d468b',
+          950: '#162c55',
+        },
+      }
+    },
   },
   plugins: [
     require('preline/plugin'),

d94891cdefbf48150c62ebc05e1509b44bce06aa
Juraj Hilje <juraj.hilje@gmail.com>
2024-06-13 11:42:42 +0200

    feat(app): update AccountVerify.vue

--- app/src/components/AccountVerify.vue
+++ app/src/components/AccountVerify.vue
@@ -8,7 +8,7 @@
         </p>
     </div>
     <div v-if="!res.is_active && !isDashboard" class="flex flex-col p-5 pb-4 my-8 bg-white">
-        <h1 class="text-3xl font-semibold text-gray-800 mb-4">Verify Your Account Email</h1>
+        <h1 class="text-3xl font-semibold text-gray-800 mb-4">Verify Your Email</h1>
         <p class="text-gray-500 mb-3">
             We have sent a 6-digit OTP code to your email address. Please enter the code below to verify your account email. Accounts with unconfirmed email address may be deleted after 7 days.
         </p>

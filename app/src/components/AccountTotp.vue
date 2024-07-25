3e95997b151944c02af967e8373b3dba72b526d5
Juraj Hilje <juraj.hilje@gmail.com>
2024-07-25 11:57:59 +0200

    feat(app): update AccountTotp.vue

--- app/src/components/AccountTotp.vue
+++ app/src/components/AccountTotp.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="mb-5">
+    <div v-if="res.id" class="mb-5">
         <h2 class="text-2xl font-semibold text-gray-800 mb-5">2-Factor Authentication</h2>
         <p class="text-sm text-gray-500 mb-5">
-            <span v-if="isEnabled()"
+            <span v-if="res.totp_enabled"
                 class="inline-flex items-center py-1.5 px-2 rounded-md text-xs font-medium bg-emerald-100 text-emerald-800">Enabled</span>
-            <span v-if="!isEnabled()"
+            <span v-if="!res.totp_enabled"
                 class="inline-flex items-center py-1.5 px-2 rounded-md text-xs font-medium bg-gray-100 text-gray-500">Disabled</span>
         </p>
         <p class="text-gray-500 mb-5">
-            When enabled, 2-factor authentication will be required when you log in.
+            When enabled, 2-factor authentication will be required when you log in.<br>
         </p>
         <div class="mb-3 max-w-xs">
-            <button v-if="!isEnabled()" @click="enable"
+            <button v-if="!res.totp_enabled" @click="enable"
                 class="bg-bluish-500 hover:bg-bluish-600 text-white font-medium py-2 px-3 focus:outline-none focus:shadow-outline" type="submit">
                 Enable
             </button>
-            <button v-if="isEnabled()" @click="disable"
+            <button v-if="res.totp_enabled" @click="disable"
                 class="bg-bluish-500 hover:bg-bluish-600 text-white font-medium py-2 px-3 focus:outline-none focus:shadow-outline" type="submit">
                 Disable
             </button>
@@ -25,19 +25,34 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-// import { userApi } from '../api/user.ts'
-// import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import { userApi } from '../api/user.ts'
+import axios from 'axios'
 
+const res = ref({ 
+    id: '',
+    totp_enabled: false
+})
 const error = ref('')
-
-const isEnabled = () => {
-    return false
-}
 
 const enable = () => {
 }
 
 const disable = () => {
 }
+
+const getUser = async () => {
+    try {
+        const response = await userApi.get()
+        res.value = response.data
+    } catch (err) {
+        if (axios.isAxiosError(err)) {
+            error.value = err.message
+        }
+    }
+}
+
+onMounted(() => {
+    getUser()
+})
 </script>

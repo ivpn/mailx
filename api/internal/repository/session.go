848352fcffd5bcbbcf02a6ab15f6cba10fb6b21a
Juraj Hilje <juraj.hilje@gmail.com>
2024-09-22 18:59:19 +0200

    feat(api): create repository/session.go

---
+++ api/internal/repository/session.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-webauthn/webauthn/webauthn"
+	"ivpn.net/email/api/internal/model"
+)
+
+func (d *Database) GetSession(ctx context.Context, token string) (webauthn.SessionData, bool, error) {
+	var session model.Session
+	q := d.Client.Where("token = ?", token).Find(&session)
+	if q.RowsAffected == 0 {
+		return webauthn.SessionData{}, false, fmt.Errorf("could not get session by token")
+	}
+
+	var sessionData webauthn.SessionData
+	err := json.Unmarshal(session.SessionData, &sessionData)
+	if err != nil {
+		return webauthn.SessionData{}, false, err
+	}
+
+	return sessionData, true, q.Error
+}
+
+func (d *Database) SaveSession(ctx context.Context, session webauthn.SessionData, token string) error {
+	sessionData, err := json.Marshal(session)
+	if err != nil {
+		return err
+	}
+
+	return d.Client.Create(&model.Session{
+		Token:       token,
+		SessionData: sessionData,
+	}).Error
+}
+
+func (d *Database) DeleteSession(ctx context.Context, token string) error {
+	return d.Client.Where("token = ?", token).Delete(&model.Session{}).Error
+}

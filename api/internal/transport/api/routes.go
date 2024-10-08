40d33a710164868db30b2f8093368f037d5a4e4d
Juraj Hilje <juraj.hilje@gmail.com>
2024-10-08 14:42:00 +0200

    feat(api): update webauthn.go

--- api/internal/transport/api/routes.go
+++ api/internal/transport/api/routes.go
@@ -38,6 +38,7 @@ func (h *Handler) SetupRoutes(cfg config.APIConfig) {
 	v1 := h.Server.Group("/v1")
 	v1.Use(auth.New(cfg, h.Cache, h.Service))
 
+	v1.Post("/register/add", limiter.New(), h.AddPasskey)
 	v1.Post("/user/sendotp", limit.New(5, 10*time.Minute), h.SendUserOTP)
 	v1.Post("/user/activate", limiter.New(), h.Activate)
 	v1.Post("/user/logout", h.Logout)
--- api/internal/transport/api/webauthn.go
+++ api/internal/transport/api/webauthn.go
@@ -74,9 +74,74 @@ func (h *Handler) BeginRegistration(c *fiber.Ctx) error {
 		IsActive: false,
 	}
 
-	// Create user or get existing one
-	_ = h.Service.PostUser(c.Context(), user)
+	// Save user
+	err = h.Service.PostUser(c.Context(), user)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// Get user
 	user, err = h.Service.GetUserByEmail(c.Context(), req.Email)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// Begin registration
+	options, sessionData, err := h.WebAuthn.BeginRegistration(user)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// Save the session
+	token := model.GenSessionToken()
+	err = h.Service.SaveSession(c.Context(), *sessionData, token, user.ID)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": ErrSaveSession,
+		})
+	}
+
+	// Set token in cookie
+	c.Cookie(auth.NewCookieTempAuthn(token, c.Path(), h.Cfg))
+
+	return c.Status(200).JSON(options)
+}
+
+// @Summary Add Passkey
+// @Description Begin Add Passkey process
+// @Tags webauthn
+// @Accept json
+// @Produce json
+// @Param email body EmailReq true "Email"
+// @Success 201 {object} SuccessRes
+// @Failure 400 {object} ErrorRes
+// @Router /register/add [post]
+func (h *Handler) AddPasskey(c *fiber.Ctx) error {
+	// Parse the request
+	req := EmailReq{}
+	err := c.BodyParser(&req)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": ErrInvalidRequest,
+		})
+	}
+
+	// Validate the request
+	err = h.Validator.Struct(req)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": ErrInvalidRequest,
+		})
+	}
+
+	// Get user
+	user, err := h.Service.GetUserByEmail(c.Context(), req.Email)
 	if err != nil {
 		return c.Status(400).JSON(fiber.Map{
 			"error": err.Error(),

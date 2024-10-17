250e99265a9f474db4cb20ac8420f5d6964aad4e
Juraj Hilje <juraj.hilje@gmail.com>
2024-10-17 12:32:40 +0200

    feat(cron): update jobs/session.go

--- api/internal/cron/jobs/session.go
+++ api/internal/cron/jobs/session.go
@@ -11,7 +11,7 @@ import (
 // Delete expired sessions
 func DeleteExpiredSessions(db *gorm.DB, cfg config.APIConfig) {
 	exp := cfg.TokenExpiration.Hours()
-	err := db.Where("created_at < NOW() - INTERVAL ? HOUR", exp).Delete(&model.Session{}).Error
+	err := db.Where("created_at < NOW() - INTERVAL ? HOUR", exp).Or("expires < NOW()").Delete(&model.Session{}).Error
 	if err != nil {
 		log.Println("Error deleting expired sessions:", err)
 		return

3ae4354cb0822093885f156c1d687b51d311a8e0
Juraj Hilje <juraj.hilje@gmail.com>
2024-10-19 17:36:20 +0200

    feat(app): update style.css

--- app/src/components/Login.vue
+++ app/src/components/Login.vue
@@ -106,6 +106,17 @@
             Here to try MailX? You need an active IVPN account.<br>
             Sign up or log in on <a class="text-bluish-500 hover:text-bluish-600" href="https://www.ivpn.net/account/">ivpn.net</a> and look for "Email Beta" in your account settings.
         </p>
+        <div class="grid grid-cols-3 gap-4 text-center mb-8">
+            <p>
+                <a class="text-bluish-500 hover:text-bluish-600" href="/privacy/">Privacy Policy</a>
+            </p>
+            <p>
+                <a class="text-bluish-500 hover:text-bluish-600" href="/tos/">Terms of Service</a>
+            </p>
+            <p>
+                <a class="text-bluish-500 hover:text-bluish-600" href="https://www.ivpn.net/">IVPN Limited</a>
+            </p>
+        </div>
     </div>
 </template>
 

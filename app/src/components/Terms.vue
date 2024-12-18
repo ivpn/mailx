ce2bde8c9a460729ef8a8e9d31b5798a1a8aa0d1
Juraj Hilje <juraj.hilje@gmail.com>
2024-12-18 14:30:38 +0100

    feat(app): create Terms.vue

---
+++ app/src/components/Terms.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container mx-auto max-w-screen-lg p-10">
+        <h1 class="text-3xl mb-5">Terms of Service</h1>
+        <p class="text-sm mb-5 text-gray-500 dark:text-gray-400">Last updated: 22 October 2024</p>
+
+        <h2 class="text-2xl mt-10 mb-5">1. Introduction</h2>
+        <p class="mb-5">These Terms and Conditions ("Terms") govern your use of MailX, an email forwarding service operated by IVPN Limited ("we," "us," or "our"). By subscribing to or using our service, you ("you" or "user") agree to abide by these Terms.</p>
+
+    </div>
+</template>

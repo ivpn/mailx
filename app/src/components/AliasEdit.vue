74a0f1f384a29cfa2a7c7ec914280e89f911ef58
Juraj Hilje <juraj.hilje@gmail.com>
2025-01-14 11:47:09 +0100

    feat(app): update AliasEdit.vue

--- app/src/components/AliasEdit.vue
+++ app/src/components/AliasEdit.vue
@@ -45,7 +45,7 @@
                                 class="appearance-none outline-none border border-gray-500 w-full py-3 px-4 text-gray-500 bg-white dark:text-gray-300 dark:bg-neutral-800 dark:border-neutral-400 leading-tight focus:border-bluish-500 mb-2"
                                 type="text">
                         </div>
-                        <div class="mb-6">
+                        <!-- <div class="mb-6">
                             <label v-bind:for="'recipient_' + alias.id"
                                 class="block text-gray-500 dark:text-gray-400 mb-3">
                                 Recipient:
@@ -55,6 +55,33 @@
                                 class="form-select py-2.5 px-4 pe-9 block w-full border border-gray-500 text-gray-500 bg-white dark:text-gray-300 dark:bg-neutral-800 dark:border-neutral-400  focus:border-bluish-500 disabled:opacity-50 disabled:pointer-events-none outline-none focus:ring-transparent">
                                 <option v-for="recipient in recipients" v-bind:value=recipient
                                     :selected="recipient == alias.recipients" :key="recipient">
+                                    {{ recipient }}
+                                </option>
+                            </select>
+                        </div> -->
+                        <div class="mb-6">
+                            <label v-bind:for="'recipient_' + alias.id"
+                                class="block text-gray-500 dark:text-gray-400 mb-3">
+                                Recipients:
+                            </label>
+                            <select
+                                v-model="selectRecipients"
+                                v-bind:id="'recipient_' + alias.id"
+                                :disabled="!recipients.length"
+                                multiple=""
+                                data-hs-select='{
+                                "placeholder": "Select multiple options...",
+                                "toggleTag": "<button type=\"button\" aria-expanded=\"false\"></button>",
+                                "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 ps-4 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-neutral-600",
+                                "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900 dark:border-neutral-700",
+                                "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800",
+                                "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-blue-500 \" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
+                                "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
+                                }' class="hidden">
+                                <option v-for="recipient in recipients"
+                                    v-bind:value=recipient
+                                    :selected="alias.recipients.includes(recipient)"
+                                    :key="recipient">
                                     {{ recipient }}
                                 </option>
                             </select>
@@ -82,6 +109,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import overlay from '@preline/overlay'
+import select from '@preline/select'
 import axios from 'axios'
 import { aliasApi } from '../api/alias.ts'
 
@@ -89,13 +117,15 @@ const props = defineProps(['alias', 'recipients'])
 const emit = defineEmits(['onEditAlias'])
 let alias = ref(Object.assign({}, props.alias))
 const recipients = ref(props.recipients)
+const selectRecipients = ref(props.recipients)
 const success = ref('')
 const error = ref('')
 
 const updateAlias = async () => {
     try {
-        const response = await aliasApi.update(alias.value.id, alias.value)
-        success.value = response.data.message
+        alias.value.recipients = selectRecipients.value.join(',')
+        const res = await aliasApi.update(alias.value.id, alias.value)
+        success.value = res.data.message
         error.value = ''
         emit('onEditAlias')
         close()
@@ -126,6 +156,7 @@ const addEvents = () => {
 
 onMounted(() => {
     overlay.autoInit()
+    select.autoInit()
     addEvents()
 })
 </script>

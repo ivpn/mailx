9026c168c4b689699f0a89d986d8d8891c127d13
Juraj Hilje <juraj.hilje@gmail.com>
2025-01-25 09:04:29 +0100

    feat(app): update AliasCreate.vue

--- app/src/events.ts
+++ app/src/events.ts
@@ -3,6 +3,8 @@ import mitt from 'mitt'
 // Define events
 type Events = {
     'user.update': { email: string }
+    'alias.create': {}
+    'alias.update': { alias: string }
 }
 
 export default mitt<Events>()

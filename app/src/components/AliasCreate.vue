963c29d7f5e65650deea9837242d4df710d47442
Juraj Hilje <juraj.hilje@gmail.com>
2025-01-30 11:39:57 +0100

    feat(app): update AliasCreate.vue

--- app/src/components/AliasCreate.vue
+++ app/src/components/AliasCreate.vue
@@ -122,9 +122,9 @@
                                 "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-bluish-600 dark:text-bluish-500 \" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
                                 "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
                                 }' class="hidden">
-                                <option v-for="(recipient, index) in recipients"
+                                <option v-for="(recipient, _) in recipients"
                                     v-bind:value=recipient
-                                    :selected="recipient == settings.recipient || index === 0"
+                                    :selected="recipient == settings.recipient"
                                     :key="recipient">
                                     {{ recipient }}
                                 </option>

0548b4d9ad6c861f7dedc108f66b0a1d6d952243
Juraj Hilje <juraj.hilje@gmail.com>
2025-02-19 14:55:03 +0100

    feat(model): update recipient.go

--- api/internal/model/recipient.go
+++ api/internal/model/recipient.go
@@ -11,9 +11,12 @@ var (
 
 type Recipient struct {
 	BaseModel
-	UserID   string `json:"-"`
-	Email    string `gorm:"unique" json:"email"`
-	IsActive bool   `json:"is_active"`
+	UserID     string `json:"-"`
+	Email      string `gorm:"unique" json:"email"`
+	IsActive   bool   `json:"is_active"`
+	PGPKey     string `json:"pgp_key"`
+	PGPEnabled bool   `json:"pgp_enabled"`
+	PGPInline  bool   `json:"pgp_inline"`
 }
 
 func GetEmails(rcps []Recipient) string {

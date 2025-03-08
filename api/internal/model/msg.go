872b6cf9f771ec03be3f4c21ad3c7d25e29c528f
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-08 18:49:29 +0100

    refactor(service): create model/msg.go

---
+++ api/internal/model/msg.go
@@ -0,0 +1,69 @@
+package model
+
+import (
+	"bytes"
+	"net/mail"
+	"strings"
+)
+
+type Msg struct {
+	From     string
+	FromName string
+	To       []string
+	Subject  string
+	Body     string
+	Type     MessageType
+}
+
+func ParseMsg(data []byte) (Msg, error) {
+	msg, err := mail.ReadMessage(bytes.NewReader(data))
+	if err != nil {
+		return Msg{}, err
+	}
+
+	subject := msg.Header.Get("Subject")
+
+	to := make([]string, 0)
+	for _, t := range strings.Split(msg.Header.Get("To"), ",") {
+		address, err := mail.ParseAddress(t)
+		if err != nil {
+			return Msg{}, err
+		}
+
+		to = append(to, address.Address)
+	}
+
+	from, err := mail.ParseAddress(msg.Header.Get("From"))
+	if err != nil {
+		return Msg{}, err
+	}
+
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(msg.Body)
+	if err != nil {
+		return Msg{}, err
+	}
+	body := buf.String()
+	msgType := Send
+
+	if isReply(msg) {
+		msgType = Reply
+	}
+
+	return Msg{
+		From:     from.Address,
+		FromName: from.Name,
+		To:       to,
+		Subject:  subject,
+		Body:     body,
+		Type:     msgType,
+	}, nil
+}
+
+func isReply(m *mail.Message) bool {
+	if m.Header.Get("In-Reply-To") != "" || m.Header.Get("References") != "" {
+		return true
+	}
+
+	return false
+}
--- api/internal/model/msg_test.go
+++ api/internal/model/msg_test.go
@@ -1,4 +1,4 @@
-package service
+package model
 
 import (
 	"net/mail"
@@ -49,7 +49,7 @@ func TestParseMessage(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := parseMessage(tt.data)
+			got, err := ParseMsg(tt.data)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("parseMessage() error = %v, wantErr %v", err, tt.wantErr)
 				return

f7dd329f4ce0215db429f7a9c32b8310b3ce3879
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-11 13:40:57 +0100

    feat: update mailer.go

--- api/internal/client/mailer/mailer.go
+++ api/internal/client/mailer/mailer.go
@@ -200,8 +200,7 @@ func (mailer Mailer) Forward(from string, name string, rcp model.Recipient, data
 		msg.SetHeader("To", rcp.Email)
 		msg.SetHeader("Subject", email.Subject)
 		msg.SetHeader("Content-Type", "multipart/encrypted; protocol=\"application/pgp-encrypted\"")
-		msg.SetHeader("Content-Description", "OpenPGP encrypted message\r\n")
-		msg.SetHeader("Content-Description", "PGP/MIME version identificatione\r\nVersion: 1\r\n")
+		msg.SetHeader("Content-Description", "OpenPGP encrypted message")
 		msg.SetHeader("Content-Disposition", "inline; filename=\"encrypted.asc\"")
 		msg.AddAlternative("application/octet-stream; name=\"encrypted.asc\"\r\n", string(armored))
 

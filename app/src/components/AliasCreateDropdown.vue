d073650bf20aec4f1c91c704f6b6a7c9e7103d5b
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-17 14:54:12 +0100

    feat(app): update AliasCreateDropdown.vue

--- app/src/components/AliasCreateDropdown.vue
+++ app/src/components/AliasCreateDropdown.vue
@@ -2,15 +2,15 @@
     <div class="hs-dropdown">
         <button id="hs-dropdown-new-alias" class="cta">
             New Alias
-            <span class="icon arrow-down text-xl icon-primary"></span>
+            <i class="icon arrow-down text-xl icon-primary"></i>
         </button>
         <div class="hs-dropdown-menu hs-dropdown-open:opacity-100 hidden" aria-labelledby="hs-dropdown-new-alias">
             <button data-hs-overlay="#modal-create-alias-false">
-                <span class="icon at bg-bd-9 text-sm"></span>
+                <i class="icon at icon-accent text-sm"></i>
                 Alias
             </button>
             <button data-hs-overlay="#modal-create-alias-true">
-                <span class="icon scan bg-bd-9 text-sm"></span>
+                <i class="icon scan icon-accent text-sm"></i>
                 Catch-all Alias
             </button>
         </div>

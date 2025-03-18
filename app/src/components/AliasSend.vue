58452cd3df6153885459bafa3d44f702d85cc478
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-18 11:26:31 +0100

    feat(app): update AliasSend.vue

--- app/src/components/AliasSend.vue
+++ app/src/components/AliasSend.vue
@@ -4,16 +4,7 @@
             <div>
                 <div>
                     <header>
-                        <h3>Send from alias</h3>
-                        <button @click="close" class="close">
-                            <span class="sr-only">Close</span>
-                            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M18 6 6 18"></path>
-                                <path d="m6 6 12 12"></path>
-                            </svg>
-                        </button>
+                        <h4>SEND FROM ALIAS</h4>
                     </header>
                     <article>
                         <div class="mb-5">
@@ -60,11 +51,11 @@
                     </article>
                     <footer>
                         <nav>
+                            <button @click="close" class="cancel">
+                                Close
+                            </button>
                             <button @click="showAddress" class="cta">
                                 Show address
-                            </button>
-                            <button @click="close" class="cta cancel">
-                                Close
                             </button>
                         </nav>
                     </footer>

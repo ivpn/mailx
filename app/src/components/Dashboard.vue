d24e87679ab78eb5d91c2dbcb96b73e4121ee20d
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-20 17:45:01 +0100

    feat(app): update Dashboard.vue

--- app/src/components/Dashboard.vue
+++ app/src/components/Dashboard.vue
@@ -3,7 +3,7 @@
         <div class="w-[224px] flex-shrink-0 fixed h-full">
             <Sidebar />
         </div>
-        <div class="ml-[224px] grow">
+        <div class="ml-[224px] grow overflow-x-hidden">
             <SubscriptionStatus />
             <router-view></router-view>
         </div>

1fff0e8b5438c2ee7a7228faacb8e395caa1e203
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-25 20:32:44 +0100

    feat(app): update Login.vue

--- app/src/components/Login.vue
+++ app/src/components/Login.vue
@@ -1,46 +1,23 @@
 <template>
     <div class="page center pt-10">
-        <h1>MailX</h1>
-        <h2 class="mb-10">
-            Email forwarding service operated by
-            <a href="https://www.ivpn.net/">IVPN</a>
-        </h2>
-        <h3>Log In</h3>
-        <form class="card-secondary center" @submit.prevent="">
-            <nav v-if="passkeySupported" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
-                <button
-                    @click="onTabChange"
-                    class="active"
-                    id="tabs-with-underline-item-1" aria-selected="true" data-hs-tab="#tabs-with-underline-1"
-                    aria-controls="tabs-with-underline-1" role="tab">
-                    Passkey
-                </button>
-                <button
-                    @click="onTabChange"
-                    id="tabs-with-underline-item-2" aria-selected="false" data-hs-tab="#tabs-with-underline-2"
-                    aria-controls="tabs-with-underline-2" role="tab">
-                    Email & Password
-                </button>
-            </nav>
-            <div v-bind:class="{ 'mt-6': passkeySupported }">
+        <form class="card-tertiary center" @submit.prevent="">
+            <div>
                 <div v-if="passkeySupported" id="tabs-with-underline-1" role="tabpanel" aria-labelledby="tabs-with-underline-item-1">
                     <div v-if="!isLoggedIn()">
-                        <div class="mb-4">
-                            <label for="email_authn">
-                                Email Address
-                            </label>
+                        <div class="mb-5">
                             <input
                                 v-model="emailAuthn"
                                 v-bind:class="{ 'error': emailAuthnError }"
                                 id="email_authn"
                                 type="email"
                                 autocomplete="email_authn"
+                                placeholder="Email Address"
                             >
                             <p v-if="emailAuthnError" class="error">Required</p>
                         </div>
                         <div class="flex items-center w-full">
                             <button :disabled="isLoading" @click="loginWithPasskey" class="cta lg full">
-                                Log In with Passkey
+                                Log in with Passkey
                             </button>
                         </div>
                         <p v-if="error" class="error mt-6">Error: {{ error }}</p>
@@ -49,29 +26,27 @@
                 <div id="tabs-with-underline-2" v-bind:class="{ 'hidden': passkeySupported }" role="tabpanel"
                     aria-labelledby="tabs-with-underline-item-2">
                     <div v-if="!isLoggedIn()">
-                        <div class="mb-4">
-                            <label for="email">
-                                Email Address
-                            </label>
+                        <h1 class="text-center text-accent">MailX</h1>
+                        <h4 class="text-center mb-8">Log in with email and password</h4>
+                        <div class="mb-3">
                             <input
                                 v-model="email"
                                 v-bind:class="{ 'error': emailError }"
                                 id="email"
                                 type="email"
                                 autocomplete="email"
+                                placeholder="Email address"
                             >
                             <p v-if="emailError" class="error">Required</p>
                         </div>
-                        <div class="mb-6">
-                            <label for="password">
-                                Password
-                            </label>
+                        <div class="mb-5">
                             <input
                                 v-model="password"
                                 v-bind:class="{ 'error': passwordError }"
                                 id="password"
                                 type="password"
                                 autocomplete="current-password"
+                                placeholder="Password"
                             >
                             <p v-if="passwordError" class="error mb-2">Required</p>
                         </div>
@@ -89,13 +64,28 @@
                         </div>
                         <div class="flex items-center w-full">
                             <button :disabled="isLoading" @click="login" class="cta lg full">
-                                Log In
+                                Log in
                             </button>
                         </div>
                         <p v-if="error" class="error mt-6">Error: {{ error }}</p>
                     </div>
                 </div>
             </div>
+            <nav v-if="passkeySupported" aria-label="Tabs" role="tablist" aria-orientation="horizontal" class="tabs-router">
+                <button
+                    @click="onTabChange"
+                    class="active"
+                    id="tabs-with-underline-item-1" aria-selected="true" data-hs-tab="#tabs-with-underline-1"
+                    aria-controls="tabs-with-underline-1" role="tab">
+                    Or use Passkey
+                </button>
+                <button
+                    @click="onTabChange"
+                    id="tabs-with-underline-item-2" aria-selected="false" data-hs-tab="#tabs-with-underline-2"
+                    aria-controls="tabs-with-underline-2" role="tab">
+                    Or use password
+                </button>
+            </nav>
             <div v-if="isLoggedIn()" class="pb-2">
                 <p>You are logged in</p>
                 <router-link to="/" tag="button" class="cta lg full">
@@ -104,7 +94,7 @@
             </div>
         </form>
         <p>
-            <router-link to="/reset/password/initiate">Forgot Your Password?</router-link>
+            <router-link to="/reset/password/initiate">Forgot password?</router-link>
         </p>
         <Footer />
     </div>

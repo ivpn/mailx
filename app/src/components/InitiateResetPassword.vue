d142ea5911fce95dc489941dbf5843c28250708c
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-27 12:27:42 +0100

    feat(app): update ResetPassword.vue

--- app/src/components/InitiateResetPassword.vue
+++ app/src/components/InitiateResetPassword.vue
@@ -3,9 +3,9 @@
         <div></div>
         <form class="card-tertiary center" @submit.prevent="initiatePasswordReset">
             <article>
+                <h1 class="text-center text-accent mb-8">MailX</h1>
+                <h4 class="text-center mb-8">Reset password</h4>
                 <div v-if="!apiSuccess">
-                    <h1 class="text-center text-accent mb-8">MailX</h1>
-                    <h4 class="text-center mb-8">Reset password</h4>
                     <div class="mb-4">
                         <input
                             v-model="email"

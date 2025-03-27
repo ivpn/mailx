f564f67d4df37236337f364b9e26c2cab4debd1c
Juraj Hilje <juraj.hilje@gmail.com>
2025-03-27 11:33:00 +0100

    feat(app): update NotFound.vue

--- app/src/components/NotFound.vue
+++ app/src/components/NotFound.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="page">
-        <div class="flex flex-col items-center text-center py-20">
+    <div class="page center">
+        <div></div>
+        <div class="text-center">
             <h1 class="text-5xl">404</h1>
             <p>Page not found</p>
             <router-link to="/" tag="button" class="cta full">
                 Go to Dashboard
             </router-link>
         </div>
+        <Footer />
     </div>
 </template>
+
+<script setup lang="ts">
+import Footer from './Footer.vue'
+</script>

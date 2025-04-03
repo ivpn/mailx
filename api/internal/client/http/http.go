d1d15208d2f55c9bf00b3584c26c97c96b1e00cc
Juraj Hilje <juraj.hilje@gmail.com>
2025-04-03 10:15:57 +0200

    feat(client): create http.go

---
+++ api/internal/client/http/http.go
@@ -0,0 +1,30 @@
+package http
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"ivpn.net/email/api/config"
+	"ivpn.net/email/api/internal/model"
+)
+
+type Http struct {
+	Cfg config.APIConfig
+}
+
+func New(cfg config.APIConfig) *Http {
+	return &Http{
+		Cfg: cfg,
+	}
+}
+
+func (http Http) PostSubscription(sub model.Subscription) error {
+	req := fiber.Post("https://api.example.net/subscription")
+	req.Set("Content-Type", "application/json")
+	req.Set("Authorization:", "Bearer "+http.Cfg.PSK)
+
+	_, _, err := req.Bytes()
+	if err != nil {
+		return err[0]
+	}
+
+	return nil
+}

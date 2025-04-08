7a8eb8972b959705ee6e96f2ed93b46d1c8a4a4d
Juraj Hilje <juraj.hilje@gmail.com>
2025-04-08 23:08:47 +0200

    feat(app): update AliasCreate.vue

--- app/src/components/AliasCreate.vue
+++ app/src/components/AliasCreate.vue
@@ -12,7 +12,7 @@
                             <span v-if="step == 2">| {{ alias.format }}</span>
                         </h4>
                         <div v-if="props.catchAll" class="hs-tooltip [--strategy:absolute]">
-                            <i class="icon info icon-primary cursor-pointer hs-tooltip-toggle"></i>
+                            <i class="icon info icon-primary hs-tooltip-toggle"></i>
                             <span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible" role="tooltip">You can create 1 catch-all alias per domain.</span>
                         </div>
                         <span v-if="!props.catchAll">{{ step }}/2</span>

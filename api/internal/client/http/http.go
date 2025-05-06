88ccccbb064b08e2144e701e65ee2ffe5986dd87
Juraj Hilje <juraj.hilje@gmail.com>
2025-05-06 20:58:48 +0200

    chore: clean up http.go

--- api/internal/client/http/http.go
+++ api/internal/client/http/http.go
@@ -1,8 +1,6 @@
 package http
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"log"
 	"net/http"
@@ -21,37 +19,6 @@ func New(cfg config.APIConfig) *Http {
 	}
 }
 
-func (h Http) SignupWebhookNet(subID string) error {
-	data := map[string]string{
-		"uuid": subID,
-	}
-
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		log.Println("Error calling signup webhook:", err)
-		return err
-	}
-
-	req, err := http.NewRequest("POST", h.Cfg.SignupWebhookURL, bytes.NewBuffer(jsonData))
-	if err != nil {
-		log.Println("Error calling signup webhook:", err)
-		return err
-	}
-
-	req.Header.Set("Authorization", "Bearer "+h.Cfg.SignupWebhookPSK)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Error calling signup webhook:", err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	return nil
-}
-
 func (h Http) SignupWebhook(subID string) error {
 	req := fiber.Post(h.Cfg.SignupWebhookURL)
 	req.Set("Content-Type", "application/json")
@@ -60,16 +27,15 @@ func (h Http) SignupWebhook(subID string) error {
 
 	status, body, err := req.Bytes()
 	if err != nil {
-		log.Println("Error calling signup webhook:", err)
-		return errors.New("Error calling signup webhook")
+		log.Printf("Error calling signup webhook: %v", err)
+		return errors.New("error calling signup webhook")
 	}
 
 	if status != http.StatusOK {
-		log.Println("Error calling signup webhook, status:", status)
-		return errors.New("Error calling signup webhook")
+		log.Printf("Error from signup webhook, status: %d, body: %s", status, string(body))
+		return errors.New("error response from signup webhook")
 	}
 
-	log.Println("Signup webhook response:", string(body))
-
+	log.Printf("Signup webhook successful, response: %s", string(body))
 	return nil
 }

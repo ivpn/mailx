73992f7d3ddde2cc7b87ce6c78da5c73fd27cb14
Juraj Hilje <juraj.hilje@gmail.com>
2025-05-06 18:23:38 +0200

    feat(client): update http.go

--- api/internal/client/http/http.go
+++ api/internal/client/http/http.go
@@ -1,7 +1,10 @@
 package http
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"ivpn.net/email/api/config"
@@ -17,10 +20,41 @@ func New(cfg config.APIConfig) *Http {
 	}
 }
 
-func (http Http) SignupWebhook(subID string) error {
-	req := fiber.Post(http.Cfg.SignupWebhookURL)
+func (h Http) SignupWebhook(subID string) error {
+	data := map[string]string{
+		"uuid": subID,
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Println("Error calling signup webhook:", err)
+		return err
+	}
+
+	req, err := http.NewRequest("POST", h.Cfg.SignupWebhookURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Println("Error calling signup webhook:", err)
+		return err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+h.Cfg.SignupWebhookPSK)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error calling signup webhook:", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func (h Http) SignupWebhookFiber(subID string) error {
+	req := fiber.Post(h.Cfg.SignupWebhookURL)
 	req.Set("Content-Type", "application/json")
-	req.Set("Authorization:", "Bearer "+http.Cfg.SignupWebhookPSK)
+	req.Set("Authorization:", "Bearer "+h.Cfg.SignupWebhookPSK)
 	req.Body([]byte(`{"uuid": "` + subID + `"}`))
 
 	_, _, err := req.Bytes()

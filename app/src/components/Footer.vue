9ecbf6826b31db6bbd0462723c2f2a70dbc9f1eb
Juraj Hilje <juraj.hilje@gmail.com>
2025-05-28 11:49:09 +0200

    feat(app): update Footer.vue

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="footer-nav flex flex-col items-center justify-center p-0">
+    <div class="footer-nav flex flex-col items-center justify-center p-0 pt-5">
         <div class="grid grid-cols-3 gap-4 text-center">
             <p class="text-sm">
                 <router-link to="/privacy">Privacy Policy</router-link>

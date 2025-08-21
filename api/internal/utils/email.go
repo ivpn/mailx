b3762662a2620bcdbc9f065793873a4c04bb396c
Juraj Hilje <juraj.hilje@gmail.com>
2025-08-21 13:35:19 +0200

    feat(utils): update email.go

--- api/internal/utils/email.go
+++ api/internal/utils/email.go
@@ -102,7 +102,8 @@ func EncryptWithPGPMIME(data []byte, fromAddr, fromName, subject, recipientEmail
 	// Part 2: ciphertext
 	body.WriteString(fmt.Sprintf("--%s\r\n", boundary))
 	body.WriteString("Content-Type: application/octet-stream; name=\"encrypted.asc\"\r\n")
-	body.WriteString("Content-Disposition: attachment; filename=\"encrypted.asc\"\r\n\r\n")
+	body.WriteString("Content-Disposition: attachment; filename=\"encrypted.asc\"\r\n")
+	body.WriteString("Content-Transfer-Encoding: 7bit\r\n\r\n")
 	body.WriteString(armored)
 	if !strings.HasSuffix(armored, "\n") {
 		body.WriteString("\r\n")

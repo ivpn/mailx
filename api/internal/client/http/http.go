66b1e69467286d8df096448ae26f2c73923984c3
Juraj Hilje <juraj.hilje@gmail.com>
2025-09-12 10:17:41 +0200

    feat(client): update http.go

--- api/internal/client/http/http.go
+++ api/internal/client/http/http.go
@@ -1,12 +1,14 @@
 package http
 
 import (
+	"encoding/json"
 	"errors"
 	"log"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"ivpn.net/email/api/config"
+	"ivpn.net/email/api/internal/model"
 )
 
 type Http struct {
@@ -38,4 +40,30 @@ func (h Http) SignupWebhook(subID string) error {
 	}
 
 	return nil
+}
+
+func (h Http) GetPreauth(ID string) (model.Preauth, error) {
+	req := fiber.Get(h.Cfg.PreauthURL + "/" + ID)
+	req.Set("Content-Type", "application/json")
+	req.Set("Accept", "application/json")
+	req.Set("Authorization", "Bearer "+h.Cfg.PreauthPSK)
+
+	var preauth model.Preauth
+	status, body, err := req.Bytes()
+	if err != nil {
+		log.Printf("Error calling preauth service: %v", err)
+		return model.Preauth{}, errors.New("error calling preauth service")
+	}
+
+	if status != http.StatusOK {
+		log.Printf("Error calling preauth service, status: %d", status)
+		return model.Preauth{}, errors.New("error response from preauth service")
+	}
+
+	if err := json.Unmarshal(body, &preauth); err != nil {
+		log.Printf("Error parsing preauth response: %v", err)
+		return model.Preauth{}, errors.New("error parsing preauth response")
+	}
+
+	return preauth, nil
 }

113b72dd8e8e60816a1f65d771c220a4772b7f14
Juraj Hilje <juraj.hilje@gmail.com>
2025-10-27 10:16:36 +0100

    feat(repository): update bounce.go

--- api/internal/repository/bounce.go
+++ api/internal/repository/bounce.go
@@ -2,8 +2,15 @@ package repository
 
 import (
 	"context"
+	"log"
+	"os"
 
 	"ivpn.net/email/api/internal/model"
+)
+
+// Configuration
+const (
+	basePath = "/var/log/bounce"
 )
 
 func (d *Database) GetBouncesByUser(ctx context.Context, userID string) ([]model.Bounce, error) {
@@ -18,4 +25,26 @@ func (d *Database) PostBounce(ctx context.Context, bounce model.Bounce) error {
 
 func (d *Database) DeleteBounceByUserID(ctx context.Context, userID string) error {
 	return d.Client.Where("user_id = ?", userID).Delete(&model.Bounce{}).Error
+}
+
+func (d *Database) SaveBounceToFile(ctx context.Context, filename string, data []byte) error {
+	filePath := basePath + "/" + filename + ".eml"
+
+	if err := os.WriteFile(filePath, data, 0600); err != nil {
+		log.Println("error writing bounce file:", err)
+		return err
+	}
+
+	return nil
+}
+
+func (d *Database) GetBounceFile(ctx context.Context, filename string) ([]byte, error) {
+	filePath := basePath + "/" + filename + ".eml"
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Println("error reading bounce file:", err)
+		return nil, err
+	}
+
+	return data, nil
 }
--- api/internal/service/bounce.go
+++ api/internal/service/bounce.go
@@ -18,6 +18,8 @@ type BounceStore interface {
 	GetBouncesByUser(context.Context, string) ([]model.Bounce, error)
 	PostBounce(context.Context, model.Bounce) error
 	DeleteBounceByUserID(context.Context, string) error
+	SaveBounceToFile(context.Context, string, []byte) error
+	GetBounceFile(context.Context, string) ([]byte, error)
 }
 
 func (s *Service) GetBouncesByUser(ctx context.Context, userID string) ([]model.Bounce, error) {
@@ -48,4 +50,24 @@ func (s *Service) DeleteBounceByUserID(ctx context.Context, userID string) error
 	}
 
 	return nil
+}
+
+func (s *Service) SaveBounceToFile(ctx context.Context, filename string, data []byte) error {
+	err := s.Store.SaveBounceToFile(ctx, filename, data)
+	if err != nil {
+		log.Printf("error saving bounce to file: %s", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) GetBounceFile(ctx context.Context, filename string) ([]byte, error) {
+	data, err := s.Store.GetBounceFile(ctx, filename)
+	if err != nil {
+		log.Printf("error getting bounce file: %s", err.Error())
+		return nil, err
+	}
+
+	return data, nil
 }

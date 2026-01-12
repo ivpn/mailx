c02419733061e986b6e07ad361ce0f9daf0bb8a1
Juraj Hilje <juraj.hilje@gmail.com>
2026-01-12 10:56:06 +0100

    feat(wxt): update wxt.config.ts

--- wxt/wxt.config.ts
+++ wxt/wxt.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'wxt'
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
-  manifest: ({ mode }) => {
+  manifest: () => {
+    const apiUrl = import.meta.env.VITE_API_URL
     return {
-      host_permissions: mode === 'development' ? ['http://0.0.0.0:3000/*'] : [],
+      host_permissions: apiUrl ? [apiUrl + '/*'] : [],
       permissions: ['storage', 'webRequest', 'activeTab'],
       web_accessible_resources: [
         {

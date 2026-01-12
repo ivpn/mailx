db14fdb277281856add8d099b80fc2e00eeac1e8
Juraj Hilje <juraj.hilje@gmail.com>
2026-01-12 11:37:33 +0100

    feat(wxt): update wxt.config.ts

--- wxt/wxt.config.ts
+++ wxt/wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
   manifest: () => {
     const apiUrl = import.meta.env.VITE_API_URL
+    console.log('API URL:', apiUrl)
     return {
       host_permissions: apiUrl ? [apiUrl + '/*'] : [],
       permissions: ['storage', 'webRequest', 'activeTab'],

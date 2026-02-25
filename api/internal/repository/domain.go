107ce0408bc279b713efc7c468a3fcd560e7a773
Juraj Hilje <juraj.hilje@gmail.com>
2026-02-25 14:02:16 +0100

    feat(repository): create domain.go

---
+++ api/internal/repository/domain.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"context"
+
+	"ivpn.net/email/api/internal/model"
+)
+
+func (d *Database) GetDomains(ctx context.Context, userID string) ([]model.Domain, error) {
+	var domains []model.Domain
+	err := d.Client.Where("user_id = ?", userID).Order("created_at desc").Find(&domains).Error
+	return domains, err
+}
+
+func (d *Database) PostDomain(ctx context.Context, domain model.Domain) (model.Domain, error) {
+	err := d.Client.Create(&domain).Error
+	return domain, err
+}
+
+func (d *Database) UpdateDomain(ctx context.Context, domain model.Domain) error {
+	return d.Client.Model(&domain).Where("user_id = ?", domain.UserID).Updates(map[string]any{
+		"name":                 domain.Name,
+		"description":          domain.Description,
+		"recipient":            domain.Recipient,
+		"from_name":            domain.FromName,
+		"enabled":              domain.Enabled,
+		"owner_verified_at":    domain.OwnerVerifiedAt,
+		"inbound_verified_at":  domain.InboundVerifiedAt,
+		"outbound_verified_at": domain.OutboundVerifiedAt,
+	}).Error
+}
+
+func (d *Database) DeleteDomain(ctx context.Context, domainID string, userID string) error {
+	return d.Client.Where("id = ? AND user_id = ?", domainID, userID).Delete(&model.Domain{}).Error
+}
+
+func (d *Database) DeleteDomainsByUserID(ctx context.Context, userID string) error {
+	return d.Client.Where("user_id = ?", userID).Delete(&model.Domain{}).Error
+}

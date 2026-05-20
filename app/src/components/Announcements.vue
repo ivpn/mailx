f94a2699ba55d1e0ed24adff77f083f8bb4f70cc
Juraj Hilje <juraj.hilje@gmail.com>
2026-05-20 11:24:30 +0200

    feat(app): update Announcements.vue

---
+++ app/src/components/Announcements.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container mx-auto max-w-screen-lg sm:p-10 p-5">
+        <p class="py-5">
+            <button @click="goBack" class="flex items-center gap-2">
+                <i class="icon arrow-left-line icon-accent"></i>
+                Back
+            </button>
+        </p>
+        <h1>News</h1>
+        <hr>
+
+        <div v-if="!loaded && !error" class="card-empty">
+            <p>Loading...</p>
+        </div>
+
+        <div v-if="error" class="card-empty">
+            <p class="text-red-500">{{ error }}</p>
+        </div>
+
+        <div v-if="loaded && list.length === 0" class="card-empty">
+            <p>No news.</p>
+        </div>
+
+        <template v-if="loaded">
+            <div v-for="(item, index) in list" :key="index">
+                <h2>{{ item.title }}</h2>
+                <div v-html="item.body"></div>
+                <p v-if="item.link">
+                    <a :href="item.link" target="_blank" rel="noopener noreferrer">{{ item.link }}</a>
+                </p>
+                <hr v-if="index < list.length - 1">
+            </div>
+        </template>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+import { announcementsApi } from '../api/announcements.ts'
+
+interface Announcement {
+    title: string
+    body: string
+    link: string
+}
+
+const router = useRouter()
+const goBack = () => router.back()
+
+const list = ref<Announcement[]>([])
+const error = ref('')
+const loaded = ref(false)
+
+onMounted(() => {
+    getList()
+})
+
+const getList = async () => {
+    try {
+        const response = await announcementsApi.getList()
+        list.value = response.data
+        loaded.value = true
+        error.value = ''
+    } catch (err) {
+        if (axios.isAxiosError(err)) {
+            error.value = err.response?.data.error || err.message
+        }
+    }
+}
+</script>
